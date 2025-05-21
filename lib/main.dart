db43c61bd55e9203f4bb0a5d5adae7c37192e401
leamlak12 <firaolgosa@gmail.com>
2025-05-21 12:21:19 +0300

    feat: Implement splash screen functionality and set it as the home route

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'firebase_options.dart';
 import 'features/auth/presentation/view/login_screen.dart';
 import 'features/auth/presentation/view/signup_screen.dart';
+import 'features/splash/presentation/view/splash_screen.dart';
 import 'core/utils/colors.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  // This is required for flutter_native_splash
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  // Keep the splash screen visible until initialization is complete
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  // Initialize Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Remove the splash screen when initialization is done
+  FlutterNativeSplash.remove();
+
   runApp(const MyApp());
 }
 
@@ -36,7 +47,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const LoginScreen(),
+      home: const SplashScreen(),
       routes: {
         '/login': (context) => const LoginScreen(),
         '/signup': (context) => const SignupScreen(),
